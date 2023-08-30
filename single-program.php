fda70f61587a17e83b590b8c1ee9b7197a189185
kyron321 <kds060902@outlook.com>
2023-08-30 12:26:30 +0100

    use new cropped images

--- single-program.php
+++ single-program.php
@@ -49,7 +49,7 @@ if($relatedProfessors->have_posts())
         $relatedProfessors->the_post(); ?>
         <li class="professor-card__list-item">
             <a href="<?php the_permalink(); ?>" class='professor-card'>
-                <img class='professor-card__image' src="<?php the_post_thumbnail_url(); ?>">
+                <img class='professor-card__image' src="<?php the_post_thumbnail_url('professorLandscape'); ?>">
                 <span class='professor-card__name'><?php the_title(); ?></spam>
             </a>    
         </li>
